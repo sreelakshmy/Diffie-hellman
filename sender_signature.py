14ef5948a7ed76dfef71b7961b964d8abc969295
Sreelakshmy R <sreelakshmy50@gmail.com>
2013-11-27 16:25:28 +0530

    Create sender_signature.py

---
+++ sender_signature.py
@@ -0,0 +1,22 @@
+import os
+import math
+import socket
+from Crypto.PublicKey import RSA
+import hashlib
+keyname='file1.txt'
+keydata = open(keyname, "r").read()
+rsa = RSA.importKey(keydata)
+secret = "abcd"
+hashed = hashlib.sha256(secret).hexdigest()
+hash1=int(hashed,16)
+print "hashed :"
+print hash1
+signature=rsa.sign(hash1,"")
+sign=str(signature)
+data1=str(sign)+str('@')+str(secret)
+host = 'localhost'
+port = 50002
+size = 1024
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host,port))
+s.send(data1)
