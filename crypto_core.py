df6484d4676dadbab44a70224f72d66472576702
Sreelakshmy R <sreelakshmy50@gmail.com>
2013-11-27 22:37:26 +0530

    Update crypto_core.py

--- crypto_core.py
+++ crypto_core.py
@@ -3,18 +3,13 @@ class RSASign(object):
         self.d = d
         self.socket = s
         self.n = n
-
-    def hashed(self,msg):
-        hashed = hashlib.sha256(msg).hexdigest()
-        hashed_message=int(hashed,16)
-        return hashed_message
-
-    def sign(self, hashed):
+    
+    def sign(self, msg):
+	    hashed_message = self.calculate_hash(msg)
         signed_message=pow(hash_m,d,n)
         return signed_message
 
     def sign_and_send(msg):
-        hashed_message = self.hashed(msg)
         signed_message = self.sign(hashed_message)
         self.s.send(str(signed_message)+'@'+str(msg))
 
@@ -26,10 +21,24 @@ class RSAVerify(object):
         self.s = s
 
     def verify(self, msg):
-        pass
+        rec_signed_msg,msg=data_c1.split('@')
+	    rec_signed_message=int(rec_signed_message)
+	    rec_hashed_message=int(pow(rec_signed_message,e,N))
+	    decr_hashed_message = self.calculate_hash(msg)
+	    if rec_hashed_message == decr_hashed:
+	        flag=1
+	    else:
+	        flag=0
+	    return flag
 
     def recv_and_verify():
         received_message = self.s.recv(1024)
         received_message = int(received_message)
         message = verify(received_message)
+        if message==1:
+            print "valid"
+        else:
+            print "invalid"
+            exit()
         return message
+    
