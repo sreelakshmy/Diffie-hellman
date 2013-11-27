224bfdbd32359cf7ff6f18c7c3b3f7ffe1aa58dc
Sreelakshmy R <sreelakshmy50@gmail.com>
2013-11-27 17:34:13 +0530

    Update recr_signature.py

--- recr_signature.py
+++ recr_signature.py
@@ -13,12 +13,10 @@ size = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host,port))
 s.listen(backlog)
-i=0
-data_c1=""
-while i<=0:
-    client, address = s.accept()
-    data_c1 = client.recv(size)
-    i=i+1
+
+client, address = s.accept()
+data_c1 = client.recv(size)
+
 
 h="00aeb5f0d7a9ec2706d919dd3ab66bfc4c59108e12e2aee7f7854a17e03ac1288be0949af11e27341dc18e2724f6a1754143aaf2c3300873ea3278f9fa69a2280b94385282ee3fedf534db52001d8a3b0c1cebaa3fcefb462bef35120aa314cacf84744ae34eddaea1276934527ef8fb5cc5e585d7adef046eb05d508268fa4eaf"
 n=int(h,16)
