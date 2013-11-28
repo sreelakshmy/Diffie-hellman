f8c56009b5c046667aefeedaf5fc44b6527f5a9b
eQuiNoX__ <equinox.71717171@gmail.com>
2013-11-28 14:13:24 +0530

    crypto_core.py modified to have proper signing and verification code

--- crypto_core.py
+++ crypto_core.py
@@ -1,39 +1,60 @@
+from common import calculate_hash
+
 DELIM="\n"
+
+from sage.all import *
+
 class RSASign(object):
-    def __init__(self, s, d, n):
-        self.d = d
+    """
+    Class that manages the sending of signed data over
+    the wire
+    """
+    def __init__(self, s, d, N):
         self.socket = s
-        self.n = n
-    
+        self.d = Integer(d)
+        self.N = Integer(N)
+
     def sign(self, msg):
-	hashed_message = calculate_hash(msg)
-        signed_message=pow(hash_message,d,n)
+        # Calculate the hash
+        hashed_message = Integer(calculate_hash(msg))
+        # Calculate the signature of the hash and return
+        signed_message=power_mod(hashed_message,self.d,self.N)
         return signed_message
 
     def sign_and_send(msg):
+        # Calculate the signature of the hash and send over the
+        # network
         signed_message = self.sign(msg)
         self.s.send(str(signed_message)+'@'+str(msg)+DELIM)
 
 
 class RSAVerify(object):
+    """
+    Class that manages the verification of messages received
+    over the wire
+    """
     def __init__(self, s, e, N):
-        self.e = e
-        self.N = N
         self.s = s
+        self.e = Integer(e)
+        self.N = Integer(N)
 
     def verify(self, msg):
-            rec_signed_msg,rec_msg=msg.split('@')
-	    rec_signed_message=int(rec_signed_msg)
-	    rec_hashed_message=int(pow(rec_signed_message,e,N))
-	    decr_hashed_message = int(calculate_hash(rec_msg),16)
-	    if rec_hashed_message == decr_hashed:
-	        return rec_msg
-	    else:
-	        exit()
+        # Split the message to get the signature and the message
+        signed_hash, msg = msg.split('@')
+        signed_hash      = Integer(signed_hash)
+        # Recalculate the hash of the message
+        recalculated_hash = Integer(calculate_hash(msg))
+        # Recompute the hash from the signed hash
+        retrieved_hash = power_mod(signed_hash, self.e, self.N)
+
+        # Compare the recomputed hash with the recalculated hash
+        if retrieved_hash != recalculated_hash:
+            return False
+        return msg
 
     def recv_and_verify():
-        received_message = self.s.recv(1024)
+        # Receive the message and verify it
+        received_message = self.s.recv(1024).strip()
         received_message = int(received_message)
         message = verify(received_message)
         return message
-    
