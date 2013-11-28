f924528df81c5b4008c8c5734a825dfa8837cbb6
eQuiNoX__ <equinox.71717171@gmail.com>
2013-11-28 22:42:59 +0530

    removing rsa_key_gen.py

--- rsa_key_gen.py
+++
@@ -1,9 +0,0 @@
-from Crypto.PublicKey import RSA
-bits=1024
-new_key = RSA.generate(bits) 
-public_key = new_key.publickey().exportKey("PEM") 
-private_key = new_key.exportKey("PEM") 
-f=open('private_key.pem', "w")
-f.write(private_key)
-f1=open('public_key.pem','w')
-f1.write(public_key)
